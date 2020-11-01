96e1eaa8fc74fe6e57af6e5db0168f078a5604db
jsingle345 <jsingle345@gmail.com>
2020-11-01 12:14:09 -0600

    commit tree d787ec477833b96c3a42e72b55f6138ae383321b

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 // Your code goes here
-document.addEventListener( "DOMContentLoaded", function () {
-    updateDOM()
-  } );
+
+
+document.addEventListener('DOMContentLoaded', function(e){
+  const par = document.querySelector('#text')
+  par.innerHTML = "This is really cool!"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
   
   
-  function updateDOM() {
-    document.getElementById( "text" )
-      .innerHTML = "This is really cool!";
-  }
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
